695ee5c814abec0d14d17390b1333087e9105c1c
VarshithaU <142087563+VarshithaU@users.noreply.github.com>
2023-09-09 15:29:27 +0530

    Add files via upload

---
+++ Datetype.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class Datetype
+{
+public static void main(String[] args)
+{
+int x;
+String name;
+double y;
+Scanner sc=new Scanner(System.in);
+y=sc.nextDouble();
+x=sc.nextInt();
+name=sc.next();
+}
+}
